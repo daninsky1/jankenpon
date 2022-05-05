fbd4c3125de89f9ef6225e28bc68ad41bcd5dabf
Daniel Santos <daninsky12@gmail.com>
2022-05-05 14:37:57 -0300

    Add play round
    Play round need getNext(), translateChoice2int() and translateChoice2str()
    to convert string choice to int to evaluate the choices and vice versa
    to return a readable string of the choices.

--- script.js
+++ script.js
@@ -3,12 +3,7 @@ const PAPER   = 1;
 const SCISSOR = 2;
 let POSSIBLE_CHOICES = 3;
 
-function computerPlay()
-{
-    return Math.floor(Math.random * POSSIBLE_CHOICES);
-}
-
-function translateChoice(choice)
+function translateChoice2str(choice)
 {
     switch (choice) {
         case ROCK:    return "ROCK";
@@ -18,7 +13,40 @@ function translateChoice(choice)
     }
 }
 
-translateChoice(0);
-translateChoice(1);
-translateChoice(2);
+function translateChoice2int(choice)
+{
+    choice = choice.toLowerCase();
+    choice = choice.trim();
+    if (choice == "rock") return ROCK;
+    else if (choice == "paper") return PAPER;
+    else if (choice == "scissor") return SCISSOR;
+}
 
+function computerPlay()
+{
+    return Math.floor(Math.random * POSSIBLE_CHOICES);
+}
+
+function getNext(selection)
+{
+    switch (selection) {
+    case ROCK: return PAPER;
+    case PAPER: return SCISSOR;
+    case SCISSOR: return ROCK;
+    default: console.assert(false);
+    }
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    if (computerSelection === playerSelection) {
+        return "DRAW!"
+    }
+    else if (getNext(playerSelection) === computerSelection) {
+        
+        return `You lose! ${translateChoice2str(computerSelection)} beats ${translateChoice2str(playerSelection)}`;
+    }
+    else {
+        return `You win! ${translateChoice2str(playerSelection)} beats ${translateChoice2str(computerSelection)}`;
+    }
+}
